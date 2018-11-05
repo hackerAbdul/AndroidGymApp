1bc6b200ea5f5619a8f353ee21326abb2aeb6f12
qaasima <qaasima@uni.coventry.ac.ul>
2018-11-05 18:18:48 +0000

    finished making the ui for the login page

--- mobile/src/main/java/com/example/abdul/gymapp/MainActivity.java
+++ mobile/src/main/java/com/example/abdul/gymapp/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.abdul.gymapp;
+import android.graphics.Color;
+import android.view.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +18,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void text_msg(View view){
+        TextView tv= (TextView) findViewById(R.id.forgottenpassword);
+
+        //alter text of textview widget
+        tv.setText("This text view is clicked");
+
+        //assign the textview forecolor
+        tv.setTextColor(Color.GREEN);
+    }
+
 }
