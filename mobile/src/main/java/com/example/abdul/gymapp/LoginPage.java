f0e2d7399d2d6b81063133eda1791dd6ac74544e
qaasima <qaasima@uni.coventry.ac.ul>
2018-12-10 00:58:12 +0000

    connecting firebase authentication

--- mobile/src/main/java/com/example/abdul/gymapp/LoginPage.java
+++ mobile/src/main/java/com/example/abdul/gymapp/LoginPage.java
@@ -5,7 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginPage extends AppCompatActivity {
@@ -13,10 +17,19 @@ public class LoginPage extends AppCompatActivity {
     private Button button;
     private TextView text;
 
+    private FirebaseAuth mAuth;
+    EditText email,password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
 
         button = findViewById(R.id.LogInButton);
         button.setOnClickListener(new View.OnClickListener() {
@@ -58,4 +71,15 @@ public class LoginPage extends AppCompatActivity {
         Intent intent = new Intent(this, Reset.class);
         startActivity(intent);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
+
+
+
 }
---
+++ mobile/src/test/java/com/example/abdul/gymapp/LoginPageTest.java
@@ -0,0 +1,20 @@
+package com.example.abdul.gymapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginPageTest {
+
+    @Test
+    public void openPre_homepage() {
+    }
+
+    @Test
+    public void openRegisterPage() {
+    }
+
+    @Test
+    public void openReset() {
+    }
+}
